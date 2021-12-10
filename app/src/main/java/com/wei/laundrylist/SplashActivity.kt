395e19551848decf07b919f68846033cbc319b4a
jiawei.hao@petkit.com <jiawei.hao@petkit.com>
2021-12-10 14:04:25 +0800

    修改.gitignore后首次提交

---
+++ app/src/main/java/com/wei/laundrylist/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.wei.laundrylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wei.laundrylist.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
---
+++ app/src/main/java/com/wei/laundrylist/model/SplashModel.kt
@@ -0,0 +1,11 @@
+package com.wei.laundrylist.model
+
+import com.cxz.kotlin.baselibs.mvp.BaseModel
+import com.wei.laundrylist.contract.SplashContract
+import io.reactivex.rxjava3.disposables.Disposable
+
+class SplashModel : BaseModel(),SplashContract.Model {
+
+
+
+}
